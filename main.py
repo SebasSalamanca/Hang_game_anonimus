572ac32a7d8eece86db5baac25840c6ac1e4b4b4
Sebastian Salamanca Bello <jsebastian1119@gmail.com>
2025-08-09 00:24:26 -0500

    Reorganice the function show in main module.

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ lives = 6
 user_level = typer.choose_user_level()
 words_level, vector = typer.pick_random_words(user_level)
 while lives > 0:
-    typer.show_user_screen(vector, lives)
     user_letter = typer.get_user_input(5)
     vector,flag = typer.compare_position_word(user_letter, words_level, vector)
     lives = typer.decrease_lives(lives,flag, words_level)
+    typer.show_user_screen(vector, lives)
     
 print("Thanks for playing.")
